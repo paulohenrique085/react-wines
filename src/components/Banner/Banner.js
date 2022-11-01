f94359abff2af0da80eac68467f228d5b9420864
Paulo Henrique Silva dos Santos <paulo.santos@mundiale.com.br>
2022-10-31 22:00:57 -0300

    feat: initial commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,16 @@
+
+import './Banner.css'
+
+const Banner = ()=>{
+    return(
+        <header className="banner">
+            
+            <img src="/images/banner.png"/>
+
+        </header>  
+    )
+    
+}
+
+
+export default Banner
---
+++ src/components/TextField/TextField.js
@@ -0,0 +1,14 @@
+import './TextField.css'
+
+const TextField = () =>{
+
+    return(
+        <div className='textField'>
+            <labebl>Nome</labebl>
+            <input/>
+        </div>
+    )
+
+}
+
+export default TextField
